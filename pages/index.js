3a7296ee08edb5eebd7d7adbe048f8dbd83545e8
George Oluoko <otienog1@gmail.com>
2022-02-16 16:23:23 +0300

    env

--- pages/index.js
+++ pages/index.js
@@ -5,9 +5,9 @@ import ProductSection from '../components/productSection';
 export default function Index() {
   return (
     <Page>
-      {process.env.NODE_ENV === 'production' && (
+      {/* {process.env.NODE_ENV === 'production' && (
         <Warning message="This is not a real e-commerce, it is just a code exercise." />
-      )}
+      )} */}
       <ProductSection />
     </Page>
   );
